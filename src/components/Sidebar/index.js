e4129075fc7c98731fb0f465770c6deb0e0bbcb7
4iLeRmax <max.zlatin@gmail.com>
2023-07-31 11:43:26 +0300

    sdfs

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -23,40 +23,44 @@ const Sidebar = ({
   onChangeTextArea,
   onModalOk,
 }) => {
-  const options = users.map(user => <Option key={user._id}>{user.fullname}</Option>);
+  const options = users.map((user) => <Option key={user._id}>{user.fullname}</Option>);
+
+  //sdfsdfsdfsdfsdf
 
   return (
-    <div className="chat__sidebar">
-      <div className="chat__sidebar-header">
+    <div className='chat__sidebar'>
+      <div className='chat__sidebar-header'>
         <div>
-          <Icon type="team" />
+          <Icon type='team' />
           <span>Список диалогов</span>
         </div>
-        <Button onClick={onShow} type="link" shape="circle" icon="form" />
+        <Button onClick={onShow} type='link' shape='circle' icon='form' />
       </div>
 
-      <div className="chat__sidebar-dialogs">
+      <div className='chat__sidebar-dialogs'>
         <Dialogs userId={user && user._id} />
       </div>
       <Modal
-        title="Создать диалог"
+        title='Создать диалог'
         visible={visible}
         onCancel={onClose}
         footer={[
-          <Button key="back" onClick={onClose}>
+          <Button key='back' onClick={onClose}>
             Закрыть
           </Button>,
           <Button
             disabled={!messageText}
-            key="submit"
-            type="primary"
+            key='submit'
+            type='primary'
             loading={isLoading}
-            onClick={onModalOk}>
+            onClick={onModalOk}
+          >
             Создать
           </Button>,
-        ]}>
-        <Form className="add-dialog-form">
-          <Form.Item label="Введите имя пользователя или E-Mail">
+        ]}
+      >
+        <Form className='add-dialog-form'>
+          <Form.Item label='Введите имя пользователя или E-Mail'>
             <Select
               value={inputValue}
               onSearch={onSearch}
@@ -67,13 +71,14 @@ const Sidebar = ({
               defaultActiveFirstOption={false}
               showArrow={false}
               filterOption={false}
-              placeholder="Введите имя пользователя или почту"
-              showSearch>
+              placeholder='Введите имя пользователя или почту'
+              showSearch
+            >
               {options}
             </Select>
           </Form.Item>
           {selectedUserId && (
-            <Form.Item label="Введите текст сообщения">
+            <Form.Item label='Введите текст сообщения'>
               <TextArea
                 autosize={{ minRows: 3, maxRows: 10 }}
                 onChange={onChangeTextArea}
